29ac03d426c4ea96cdea95d7564f4470d527b7a1
Ania <ania.c.skudlarska@gmail.com>
2021-04-07 11:30:17 -0400

    Input functionality

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from PIL import Image
 import numpy as np
 
-r = range(1024)
+
+
+var1 = int(input("Enter a value for var 1 (37 is default): "))
+var2 = int(input("Enter a value for var 2 (7 is default): "))
+var3 = int(input("Enter a value for var 3 (higher than 100):  "))
+
+var4 = int(input("Enter a size for the bitmap (Default is 1024): "))
+
+var5 = int(input("Enter an int 1-8: "))
+
+r = range(1024) #make this a var
 
 x, y = np.meshgrid(r, r)
-i = (1 == (abs(x+y)^abs(x-y)+1)**12 % 7) * 255
+
+i = (var5 == (abs(x+y)^abs(x-y)+1)**var1 % var2) * var3
+
+
+
 Image.fromarray(i).convert('L').save('1.png')
+im = Image.open('1.png')
+im.show()
 
 
-showpic = Image.open("1.png")
-showpic.show()
-
-
-#TODO: Make a GUI and input prompts so that a user can mess with this
-#TODO: Variables for inputs, user prompts,
-#Test Change
-#Does my git work now?
-#Decoupling an old user account, want to get this solved for the future so its not a problem later
+#This is the vanilla code
+#We want to
