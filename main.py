a04a8dbbadf0fda7432c71f0bc378e5ef3cd1f17
Rosalind Chapman <chapman.rosalind@gmail.com>
2021-04-07 10:42:06 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import numpy as np
+
+r = range(1024)
+
+x, y = np.meshgrid(r, r)
+i = (1 == (abs(x+y)^abs(x-y)+1)**12 % 7) * 255
+Image.fromarray(i).convert('L').save('1.png')
+
+
+showpic = Image.open("1.png")
+showpic.show()
+
