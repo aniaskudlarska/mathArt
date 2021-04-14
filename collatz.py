0112dabeccca639eaa3fc834137e57d7ad63ff2f
Ania <41090467+aniaskudlarska@users.noreply.github.com>
2021-04-14 17:35:47 -0400

    rewriting function for easier graphing

--- collatz.py
+++ collatz.py
@@ -1,21 +1,24 @@
 import sys
+import matplotlib
+
 #Rewriting my old Collatz code
 #Make a graph of this and make it pretty
 
-def Collatz(num):
-    print(num)
-    if num == 1:
-        return("Completed")
+def Collatz(num, counter=0):
 
-    elif num != 1:
+    while num != 1:
         if num % 2==0:
             num = num // 2
+            counter += 1
+
         elif num % 2 == 1:
             num = (3*num) +1
-        Collatz(num)
+            counter += 1
 
-    return("Done")
+    return("Reached end in " + str(counter) + " steps")
 
 print(Collatz(22))
+
+#this keeps track of how many steps, so we can graph how many steps Y it takes to collatz a value X
 
 
